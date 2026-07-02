78d027bc1892b646d91cc457483ab67ac68a31a5
Sławomir Czerwiński <slawomir@czerwinski.it>
2026-07-02 14:53:47 +0100

    Truncate long report summary (#21)

--- summarise-report.sh
+++ summarise-report.sh
@@ -63,3 +63,10 @@ do
 
   append_line "${IDE}" "${IDE_REPORT_DIR}/${VERDICT_FILE}"
 done
+
+REPORT_SIZE=$(stat -c%s "${REPORT_PATH}")
+
+if [ "${REPORT_SIZE}" -gt 65535 ]
+then
+  truncate --size=65535 "${REPORT_PATH}"
+fi
